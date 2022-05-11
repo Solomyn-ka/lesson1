fc737f3831cef537e9214ed32a97c5e822978dd1
Solomyn-ka <mia123ravok@gmail.com>
2022-05-11 18:14:40 +0200

    lesson 11 update

--- lesson11/task7.js
+++ lesson11/task7.js
@@ -1,17 +1,63 @@
 // - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
-
-
 // створити пустий масив, наповнити його 10 об'єктами new User(....)
-//
 // - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
-//
 // - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
 //
+// function User(id, name, surname, email, phone) {
+//     this.id = id;
+//     this.name = name;
+//     this.surname = surname;
+//     this.email = email;
+//     this.phone = phone;
+// }
+// let array = [];
+// let user1 = new User(5, 'Solomiia', 'Ravok', 'mia@gmail.com', 4244422242);
+// let user2 = new User(6, 'Solomiia', 'Ravok', 'mia@gmail.com', 4244422242);
+// let user3 = new User(1, 'Solomiia', 'Ravok', 'mia@gmail.com', 4244422242);
+// let user4 = new User(2, 'Solomiia', 'Ravok', 'mia@gmail.com', 4244422242);
+// let user5 = new User(4, 'Solomiia', 'Ravok', 'mia@gmail.com', 4244422242);
+// let user6 = new User(3, 'Solomiia', 'Ravok', 'mia@gmail.com', 4244422242);
+// let user7 = new User(7, 'Solomiia', 'Ravok', 'mia@gmail.com', 4244422242);
+// let user8 = new User(8, 'Solomiia', 'Ravok', 'mia@gmail.com', 4244422242);
+// let user9 = new User(10,'Solomiia', 'Ravok', 'mia@gmail.com', 4244422242);
+// let user10 = new User(9,'Solomiia', 'Ravok', 'mia@gmail.com', 4244422242);
+// array = [user1, user2, user3, user4, user5, user6, user7, user8, user9, user10];
+// let users = array.filter(value => value.id % 2 ===0);
+// console.log(users);
+// let users1 = array.sort((a, b) => a.id - b.id);
+// console.log(users1);
+
 // - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
 // створити пустий масив, наповнити його 10 об'єктами Client
 // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 //
 //
+// class Client {
+//     constructor(id, name, surname, email, phone, order) {
+//         this.id = id;
+//         this.name = name;
+//         this.surname = surname;
+//         this.email = email;
+//         this.phone = phone;
+//         this.order = order;
+//     }
+// }
+//
+//
+// let clients = [];
+// let user1 = new Client(3, 'Solomiia', 'Ravok', 'mia@gmail.com', '4244422242',['water','milk']);
+// let user2 = new Client(10, 'Solomiia', 'Ravok', 'mia@gmail.com', '4244422242',['water']);
+// let user3 = new Client(5, 'Solomiia', 'Ravok', 'mia@gmail.com', '4244422242',['water','sticks']);
+// let user4 = new Client(9, 'Solomiia', 'Ravok', 'mia@gmail.com', '4244422242',['water','ananas','sticks']);
+// let user5 = new Client(1, 'Solomiia', 'Ravok', 'mia@gmail.com', '4244422242',['water','banan']);
+// let user6 = new Client(7, 'Solomiia', 'Ravok', 'mia@gmail.com', '4244422242',['water','kokos','banan','kakao']);
+// let user7 = new Client(6, 'Solomiia', 'Ravok', 'mia@gmail.com', '4244422242',['water']);
+// let user8 = new Client(8, 'Solomiia', 'Ravok', 'mia@gmail.com', '4244422242',['water','kokos']);
+// let user9 = new Client(2,'Solomiia', 'Ravok', 'mia@gmail.com', '4244422242',['water','banan','cheese']);
+// let user10 = new Client(4,'Solomiia', 'Ravok', 'mia@gmail.com', '4244422242',['water','kakao']);
+// clients = [user1, user2, user3, user4, user5, user6, user7, user8, user9, user10];
+// let Clients = clients.sort((a, b) => a.order.length - b.order.length);
+// console.log(Clients);
 
 // - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
 // -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
